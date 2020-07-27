46e5619cb87afd694e557ff38a68081a6e48514d
Hesham Ghander <hisham011232@ci.suez.edu.eg>
2020-07-27 17:34:18 +0200

    modified CustomerController class to get customers list from DAO

--- src/com/allegiancemd/springdemo/controller/CustomerController.java
+++ src/com/allegiancemd/springdemo/controller/CustomerController.java
@@ -1,15 +1,24 @@
 package com.allegiancemd.springdemo.controller;
 
+import com.allegiancemd.springdemo.dao.CustomerDAO;
+import com.allegiancemd.springdemo.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+    @Autowired
+    private CustomerDAO customerDAO;
     @RequestMapping("/list")
     public String customersList(Model model) {
+        List <Customer> customerList = customerDAO.getCustomers();
+        model.addAttribute("customers",customerList);
         return "list-customers";
     }
 }

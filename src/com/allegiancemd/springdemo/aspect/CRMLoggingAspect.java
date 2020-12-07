b4e84159ce259984ca07fa1e64a3201e7907fb4d
Hisham <hisham011232@ci.suez.edu.eg>
2020-12-07 16:27:28 +0200

    added afterReturningMethod to display returning result of methods using @AfterReturning annotations

--- src/com/allegiancemd/springdemo/aspect/CRMLoggingAspect.java
+++ src/com/allegiancemd/springdemo/aspect/CRMLoggingAspect.java
@@ -1,6 +1,7 @@
 package com.allegiancemd.springdemo.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -36,8 +37,17 @@ public class CRMLoggingAspect {
     @Before("forAppFlow()")
     private void beforeMethod(JoinPoint joinPoint) {
         logger.info("============> Running in " + joinPoint.getSignature().toShortString() + " method =======");
+        Object[] args = joinPoint.getArgs();
+        for (Object arg : args) {
+            logger.info("=====> argument is : " + arg);
+        }
     }
 
-//    Add @AfterRetuning advice
+    //    Add @AfterRetuning advice
+    @AfterReturning(pointcut = "forAppFlow()", returning = "result")
+    private void afterReturningMethod(JoinPoint joinPoint, Object result) {
+        logger.info("============> Running after retuning " + joinPoint.getSignature().toShortString() + " method =======");
+        logger.info("the retuning value is: " + result);
+    }
 
 }
